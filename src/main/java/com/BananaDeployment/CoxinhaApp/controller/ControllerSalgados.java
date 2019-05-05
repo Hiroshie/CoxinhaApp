8107a7b39156354a10d17b2fe0620319f1c37c0a
LuanC03 <luan.bezerra@ccc.ufcg.edu.br>
2019-05-04 21:18:15 -0300

    Adicionando Funcoes

--- src/main/java/com/BananaDeployment/CoxinhaApp/controller/ControllerSalgados.java
+++ src/main/java/com/BananaDeployment/CoxinhaApp/controller/ControllerSalgados.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.BananaDeployment.CoxinhaApp.model.Salgado;
 import com.BananaDeployment.CoxinhaApp.repository.SalgadosRepository;
@@ -42,8 +45,35 @@ public class ControllerSalgados {
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Salgado adicionar(@Valid @RequestBody Salgado salgado) {
+		Optional<Salgado> salgadoExistente = salgados.findByNomeAndValor(salgado.getNome(), salgado.getValor());
+		if(salgadoExistente.isPresent())
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
 		return salgados.save(salgado);
 	}
 	
+	@DeleteMapping	
+	public HttpStatus deleteSalgadoById(@PathVariable Long id) {
+		Optional<Salgado> salgado = salgados.findById(id);
+		HttpStatus httpStatus = HttpStatus.OK;
+		if(salgado.isPresent()) 
+			salgados.delete(salgado.get());
+		else
+			httpStatus = HttpStatus.NOT_FOUND;
+		if(salgado.isPresent())
+			httpStatus = HttpStatus.NOT_MODIFIED;
+		return httpStatus;
+	}
 	
+	@DeleteMapping	
+	public HttpStatus deleteSalgadoByNomeAndValor(@PathVariable String nome, @PathVariable double valor) {
+		Optional<Salgado> salgado = salgados.findByNomeAndValor(nome, valor);
+		HttpStatus httpStatus = HttpStatus.OK;
+		if(salgado.isPresent()) 
+			salgados.delete(salgado.get());
+		else
+			httpStatus = HttpStatus.NOT_FOUND;
+		if(salgado.isPresent())
+			httpStatus = HttpStatus.NOT_MODIFIED;
+		return httpStatus;
+	}
 }
--- src/main/java/com/BananaDeployment/CoxinhaApp/repository/SalgadosRepository.java
+++ src/main/java/com/BananaDeployment/CoxinhaApp/repository/SalgadosRepository.java
@@ -1,9 +1,12 @@
 package com.BananaDeployment.CoxinhaApp.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.BananaDeployment.CoxinhaApp.model.Salgado;
 
 public interface SalgadosRepository extends JpaRepository<Salgado, Long>{
 
+	Optional<Salgado> findByNomeAndValor(String nome, double valor);
 }
